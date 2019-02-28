95452be831633444d43e1295db2b0b551f9be977
张佩 <pei5292@qq.com>
2019-02-28 17:11:52 +0800

    解决了商品详情页面

--- public/m/js/product.js
+++ public/m/js/product.js
@@ -1,14 +1,19 @@
 $(function () {
-    /* 区域滚动 */
-    mui('.mui-scroll-wrapper').scroll({
-        indicators: false, //是否显示滚动条
-    }); 
     /* 获取url地址传递的参数 */
     let urlParams = lt.getParamsByURL();
     getProductData({id:urlParams.productId}, function (data) {
         setTimeout(() => {
+            /* 渲染页面 */
             let productTemplate = template('productTemplate', {list:data});
             $('.mui-scroll').html(productTemplate);
+            /* 区域滚动 */
+            mui('.mui-scroll-wrapper').scroll({
+                indicators: false, //是否显示滚动条
+            }); 
+            /* 初始化轮播图 */
+            mui('.mui-slider').slider({
+                interval: 3000
+            });
         }, 1000);
     });
 });
