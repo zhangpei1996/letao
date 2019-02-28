ae25e61ea7126ac0aabcdad5b681ce55e431b91a
张佩 <pei5292@qq.com>
2019-02-28 19:47:49 +0800

    完成加入购物车功能

--- public/m/js/product.js
+++ public/m/js/product.js
@@ -14,6 +14,59 @@ $(function () {
             mui('.mui-slider').slider({
                 interval: 3000
             });
+
+            /* 尺码的选择 */
+            $('.btn_size').on('tap', function () {
+                $(this).addClass('now').siblings('span').removeClass('now');
+            });
+            /* 数量的选择 */
+            $('.p_number span').on('tap', function () {
+                let $input = $(this).siblings('input');
+                let currentNum = $input.val();
+                if ($(this).hasClass('jian')) {
+                    if (currentNum == 0) {
+                        return;
+                    }
+                    currentNum--;
+                } else if ($(this).hasClass('jia')) {
+                    if (currentNum >= parseInt($input.attr('data-max'))) {
+                        /* 消息框点击的时候会消失  正好和+号重叠在一起(击穿--点击穿透 tap特有的)  使用延时处理 */
+                        setTimeout(() => {
+                            mui.toast('库存不足');
+                        }, 100);
+                        return;
+                    }
+                    currentNum++;
+                }
+                $input.val(currentNum);
+            });
+            /* 加入购物车 */
+            $('.btn_addCart').on('tap', function () {
+                /* 数据校验 */
+                let $changeBtn = $('.btn_size.now');
+                if (!$changeBtn.length) {
+                    mui.toast('请选择尺码');
+                    return;
+                }
+                let $size = $changeBtn.text();
+                let $num = $('.p_number input').val();
+                if ($num <= 0) {
+                    mui.toast('请选择数量');
+                    return;
+                }
+                /* 提交数据 */
+                $.post('/cart/addCart', {
+                    productId: urlParams.productId,
+                    num: $num,
+                    size: $size
+                }, function (res) {
+                    console.log(res);
+                    if (res.error == 400) {
+                        /* 代表未登录 跳转到登录页 并且把当前地址传递给登录页面 登陆成功后跳转回原页面 */
+                        return;
+                    }
+                });
+            });
         }, 1000);
     });
 });
